3a58aa1d5a803991e530d2553007f308ef40c885
Rialga <febrialgani@gmail.com>
2021-05-25 00:40:10 +0700

    add some Package

--- app/src/main/java/com/example/presensi/Activity/MainActivity.java
+++ app/src/main/java/com/example/presensi/Activity/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.presensi;
+package com.example.presensi.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.presensi.R;
 
 public class MainActivity extends AppCompatActivity {
 
